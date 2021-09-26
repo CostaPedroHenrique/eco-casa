5407166ac9e0e0a033480162fd068b56c7fcf5c8
CostaPedroHenrique <devpedrohenrique@gmail.com>
2021-09-26 19:48:09 -0300

    feat: added css styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,24 @@
 import styles from './styles.module.scss'
+import logoImg from '../../assets/images/logo.png'
 
 export default function Header(){
     return (
-        <>
+        <div className={styles.main}>
         <div className={styles.navBar}>
             <div>
-                Eco Casa
+                <img width="200vw" src={logoImg.src} />
             </div>
             <div className={styles.navItems}>
-                <a href="https://google.com">Teste</a>
-                <a href="https://google.com">Teste</a>
-                <a href="https://google.com">Teste</a>
-                <a href="https://google.com">Teste</a>
+                <a href="/">Início</a>
+                <a href="/signup">Criar conta</a>
+                <a href="/login">Entrar</a>
+                <a href="/dashboard">Dashboard</a>
             </div>
         </div>
         <div className={styles.header}>
-            <h1>Eco Casa</h1>
+            <h1>E-coleta</h1>
             <span>Um novo jeito de descartar corretamente seus resíduos</span>
         </div>
-        </>
+        </div>
     )
 }
