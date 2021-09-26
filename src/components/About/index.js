d0dd5bbd62be5f73ae7bce9b7ec0174609d4cb0d
CostaPedroHenrique <devpedrohenrique@gmail.com>
2021-09-26 20:05:36 -0300

    refactor: changed text of about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ export default function About(){
             <div className={styles.content}>
                 <Image className={styles.contentImg} src={trashImg}/>
                 <div className={styles.contentText}>
-                    <h3>Sobre o que é o desafio?</h3>
+                    <h3 style={{fontWeight: 700, fontSize: 18}}>Sobre o que é o desafio?</h3>
                     <p>O plástico é o principal poluente dos oceanos e em alguns locais já se tornou até parte da composição do oceano.</p>
                     <p>Grande parte desse plástico são garrafas pets não descartadas corretamente. Apesar de ser 100% reciclável, ainda há muita gente que descarte as garrafas PET no meio ambiente em vez de as encaminhar corretamente para a coleta seletiva.</p>
                     <p>Isso acaba gerando diversos problemas, tanto nos oceanos quanto na terra. Entre esses problemas os principais são:</p>
@@ -19,12 +19,8 @@ export default function About(){
             </div>
             <div className={styles.content}>
                 <div className={styles.contentText}>
-                    <h3>Sobre o que é o desafio?</h3>
-                    <p>O plástico é o principal poluente dos oceanos e em alguns locais já se tornou até parte da composição do oceano.</p>
-                    <p>Grande parte desse plástico são garrafas pets não descartadas corretamente. Apesar de ser 100% reciclável, ainda há muita gente que descarte as garrafas PET no meio ambiente em vez de as encaminhar corretamente para a coleta seletiva.</p>
-                    <p>Isso acaba gerando diversos problemas, tanto nos oceanos quanto na terra. Entre esses problemas os principais são:</p>
-                    <p>Redução da biodiversidade nos oceanos, aumento da quantidade de lixo nos aterros, aumento da quantidade de algas no oceano e a morte de aves e mamíferos marinhos.</p>
-                    <p>Visando reduzir uma parcela de todo esse plástico que acaba nos oceanos, ruas e aterros desenvolvemos um sistema que visa incentivar o descarte seletivo, inicialmente de garrafas pets. </p>
+                    <h3 style={{fontWeight: 700, fontSize: 18}}>Nosso Projeto</h3>
+                    <p>A nossa lixeira Automatizada, cuja função é armazenar e quantificar resíduos  de Tereftalato de Polietileno – PET, através de componentes eletrônicos e controle por Arduíno. Para utilizar essa lixeira, basta realizar previamente um cadastro com seus dados e definir um login para ter acesso. Além de ser uma lixeira que coleta seus resíduos e faz a destinação correta para reciclagem, ela também converte seu lixo em pontos após cada descarte, possibilitando o acesso a benefícios como por exemplo o IPTU verde ou até mesmo descontos em empresas parceiras desse projeto.</p>
                 </div>
                 <Image className={styles.contentImg} src={trashExampleImg}/>
             </div>
